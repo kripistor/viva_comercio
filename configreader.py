0b9b98dd1bc391ad641968f791f6b8d0f909ce00
kripistor <liss01062004l@gmail.com>
2023-10-09 22:48:37 +0300

    add: docker file, main database operations

---
+++ configreader.py
@@ -0,0 +1,10 @@
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    postgres_dsn: PostgresDsn
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+config = Config()
